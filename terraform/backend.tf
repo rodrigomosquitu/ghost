f3688e5ba84a0986901042f1bd8991e8b7290019
rodrigomosquitu <rodrigomosquitu@gmail.com>
2021-12-03 13:56:33 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,5 @@ terraform {
     bucket  = "nord-cloud-penguin"
     region  = "us-east-1"
     key     = "tfstate/terraform.tfstate"
-    access_key = "AKIATP75CTMU6C4MSZ2F"
-    secret_key = "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"
   }
 }
